98e472efc7eb0c85b2c325b8134d52d46c5160f8
Vasco Rodrigues Duarte <duarte-vasco@outlook.com>
2021-11-12 19:15:45 +0000

    Fix Movement

--- So_Calafrios/Assets/Scripts/Player/PlayerMovement.cs
+++ So_Calafrios/Assets/Scripts/Player/PlayerMovement.cs
@@ -7,17 +7,8 @@ public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] public CharacterController controller = default;
     [SerializeField] public float speed = 12f;
-    private bool velocity = true;
     private float x;
     private float z;
-    private float y;
-    private Rigidbody rb;
-
-     private void Start()
-    {
-        // rb = GetComponent<Rigidbody>();
-        // rb.useGravity = true;
-    }
 
     /// <summary>
     /// Private method called every frame.
@@ -27,15 +18,13 @@ public class PlayerMovement : MonoBehaviour
         // Get the input for the movement.
         x = Input.GetAxis("Horizontal");
         z = Input.GetAxis("Vertical");
-
-        // Update player position with the movement input.
-        Vector3 move = (transform.right * x) + (transform.forward * z) + 
-            (transform.up * y);
-        controller.Move(move * speed * Time.deltaTime);
     }
 
     private void FixedUpdate()
     {
-        
+        // Update player position with the movement input.
+        Vector3 move = (transform.right * x * speed) + 
+            (transform.forward * z * speed) + (transform.up * -15);
+        controller.Move(move * Time.deltaTime);
     }
 }
