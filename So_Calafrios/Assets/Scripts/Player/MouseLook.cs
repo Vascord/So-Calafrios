17864ee7238777e06b6ba1f77e9f4f9eb5b49e6b
Vascord <duarte-vasco@outlook.com>
2021-11-12 18:25:23 +0000

    Add Player

---
+++ So_Calafrios/Assets/Scripts/Player/MouseLook.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine.UI;
+using UnityEngine;
+
+/// <summary>
+/// Class that permits the movement of the camera to be locked to the mouse
+/// movement.
+/// </summary>
+public class MouseLook : MonoBehaviour
+{
+    [SerializeField] private float mouseSensitivity = default;
+    [SerializeField] private Transform playerBody = default;
+    // [SerializeField] private Slider slider = default;
+    private float xRotation = 0f;
+
+    /// <summary>
+    /// Private method called before the first frame.
+    /// </summary>
+    private void Start()
+    {
+        // Lock cursor in game window (press ESC to see cursor).
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    /// <summary>
+    /// Private method called every frame.
+    /// </summary>
+    private void Update()
+    {
+        // Gets X and Y angle axis.
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity *
+            Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity *
+            Time.deltaTime;
+
+        /* Rotates player vision in Y axis and limits the rotation in 80 and 
+        -80 degrees. */
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -80f, 80f);
+        transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+
+        // Rotates player body with the X camera axis.
+        playerBody.Rotate(Vector3.up * mouseX);
+    }
+
+    // /// <summary>
+    // /// Public method which puts the slider value as the mouse sensibility.
+    // /// </summary>
+    // public void MouseSensibility()
+    // {
+    //     mouseSensitivity = slider.value;
+    // }
+}
---
+++ So_Calafrios/Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// Class which manages the movement of the player and his effects.
+/// </summary>
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] public CharacterController controller = default;
+    [SerializeField] public float speed = 12f;
+    private bool velocity = true;
+    private float x;
+    private float z;
+    private float y;
+    private Rigidbody rb;
+
+     private void Start()
+    {
+        // rb = GetComponent<Rigidbody>();
+        // rb.useGravity = true;
+    }
+
+    /// <summary>
+    /// Private method called every frame.
+    /// </summary>
+    private void Update()
+    {
+        // Get the input for the movement.
+        x = Input.GetAxis("Horizontal");
+        z = Input.GetAxis("Vertical");
+
+        // Update player position with the movement input.
+        Vector3 move = (transform.right * x) + (transform.forward * z) + 
+            (transform.up * y);
+        controller.Move(move * speed * Time.deltaTime);
+    }
+
+    private void FixedUpdate()
+    {
+        
+    }
+}
