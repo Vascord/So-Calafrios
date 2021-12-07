74b619afbb1501e7683d970a7680640f2a47863b
Vasco Rodrigues Duarte <duarte-vasco@outlook.com>
2021-12-07 15:25:13 +0000

    Add limit to EMP

--- So_Calafrios/Assets/Scripts/Player/ThrowEMP.cs
+++ So_Calafrios/Assets/Scripts/Player/ThrowEMP.cs
@@ -1,18 +1,28 @@
 using UnityEngine;
+using TMPro;
 
 public class ThrowEMP : MonoBehaviour
 {
     [SerializeField] private GameObject empGrenade;
     [SerializeField] private GameObject mainCamera;
+    [SerializeField] private TextMeshProUGUI empNumberText;
+    [SerializeField] private int empNumber;
+
+    void Start()
+    {
+        empNumberText.text = empNumber.ToString();
+    }
 
     /// <summary>
     /// Private method called every frame.
     /// </summary>
     void Update()
     {
-        if(Input.GetButtonDown("ThrowEMP"))
+        if(Input.GetButtonDown("ThrowEMP") && empNumber != 0)
         {
             ThrowGrenade();
+            empNumber--;
+            empNumberText.text = empNumber.ToString();
         }
     }
 
