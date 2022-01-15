d0dd4f5ec8a1df791e629004220bde7a52e4c2aa
Vasco Rodrigues Duarte <duarte-vasco@outlook.com>
2022-01-15 18:28:16 +0000

    Add options save and load

--- So_Calafrios/Assets/Scripts/Interface/Options.cs
+++ So_Calafrios/Assets/Scripts/Interface/Options.cs
@@ -14,6 +14,7 @@ public class Options : MonoBehaviour
     [SerializeField] private Slider lightSlider = default;
     [SerializeField] private Toggle toggle = default;
     [SerializeField] private TMP_Dropdown resolutionDropdown = default;
+    [SerializeField] private TMP_Dropdown qualityDropdown = default;
     private Resolution[] resolutions;
     private int currentresolutionIndex;
 
@@ -24,6 +25,8 @@ public class Options : MonoBehaviour
         float audio;
         audioMixer.GetFloat("volume", out audio);
         volumeSlider.value = audio;
+
+        qualityDropdown.value = QualitySettings.GetQualityLevel();
 
         toggle.isOn = Screen.fullScreen;
 
@@ -43,16 +46,11 @@ public class Options : MonoBehaviour
 
         for (int i = 0; i < resolutions.Length; i++)
         {
-            string option = resolutions[i].width + "x" + resolutions[i].height;
+            string option = resolutions[i].width + "x" + resolutions[i].height +
+            "&" + resolutions[i].refreshRate;
             options.Add(option);
 
-            if (resolutions[i].width == Screen.currentResolution.width &&
-                resolutions[i].height == Screen.currentResolution.height &&
-                resolutions[i].refreshRate == 
-                Screen.currentResolution.refreshRate)
-            {
-                currentresolutionIndex = i;
-            }
+            currentresolutionIndex = i;
         }
         resolutionDropdown.AddOptions(options);
         resolutionDropdown.value = currentresolutionIndex;
@@ -62,34 +60,38 @@ public class Options : MonoBehaviour
     public void SetResolution(int resolutionIndex)
     {
         Resolution resolution = resolutions[resolutionIndex];
-        Screen.SetResolution(resolution.width,
-            resolution.height,
+        Screen.SetResolution(resolution.width, resolution.height, 
             Screen.fullScreen);
     }
 
     public void SetVolume(float volume)
     {
         audioMixer.SetFloat("volume", volume);
+        PlayerPrefs.SetFloat("volume", volume);
     }
 
     public void SetMouseSpeed(float value)
     {
         mouse.mouseSensitivity = value;
+        PlayerPrefs.SetFloat("mouse sensitivity", value);
     }
 
     public void SetLight(float value)
     {
         sceneLight.intensity = value;
+        PlayerPrefs.SetFloat("brightness", value);
     }
 
     public void SetQuality(int qualityIndex)
     {
         QualitySettings.SetQualityLevel(qualityIndex);
+        PlayerPrefs.SetInt("quality", qualityIndex);
     }
 
     public void SetFullscreen()
     {
         Screen.fullScreen = toggle.isOn;
+        PlayerPrefs.SetString("screen", toggle.isOn.ToString());
     }
 }
 
---
+++ So_Calafrios/Assets/Scripts/System/LoadOptionsPrefs.cs
@@ -0,0 +1,47 @@
+using UnityEngine.Audio;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using System;
+
+public class LoadOptionsPrefs : MonoBehaviour
+{
+    [SerializeField] private AudioMixer audioMixer = default;
+    [SerializeField] private MouseLook mouse = default;
+    [SerializeField] private Light sceneLight = default;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if(SceneManager.GetActiveScene().buildIndex == 0)
+        {
+            if(PlayerPrefs.GetInt("quality") != 0)
+            {
+                QualitySettings.SetQualityLevel(PlayerPrefs.GetInt("quality"));
+            }
+
+            if(PlayerPrefs.GetFloat("volume") != 0)
+            {
+                audioMixer.SetFloat("volume", PlayerPrefs.GetFloat("volume"));
+            }
+
+            if(PlayerPrefs.GetString("screen") != null)
+            {
+                Screen.fullScreen = Convert.ToBoolean(PlayerPrefs.
+                    GetString("screen"));
+            }
+        }
+        else
+        {
+            if(PlayerPrefs.GetFloat("mouse sensitivity") != 0)
+            {
+                mouse.mouseSensitivity = PlayerPrefs.
+                    GetFloat("mouse sensitivity");
+            }
+            if(PlayerPrefs.GetFloat("brightness") != 0)
+            {
+                sceneLight.intensity = PlayerPrefs.GetFloat("brightness");
+            }
+        }
+    }
+}
