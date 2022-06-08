35105fc774c8dfb43b197e5de729aac8a88a701c
Vasco Rodrigues Duarte <duarte-vasco@outlook.com>
2022-06-08 21:14:48 +0100

    Bug fixes and Player Taller

--- So_Calafrios/Assets/Scripts/Interface/ChangeScene.cs
+++ So_Calafrios/Assets/Scripts/Interface/ChangeScene.cs
@@ -7,12 +7,21 @@ using UnityEngine.SceneManagement;
 public class ChangeScene : MonoBehaviour
 {
     [SerializeField] private int sceneNumber;
+
     /// <summary>
     /// Public method called to return to the menu.
     /// </summary>
-    public void Return()
+    public void ChangeToNumberScene()
     {
-        // Returns to Menu
+        if(sceneNumber == 0)
+        {
+            Cursor.lockState = CursorLockMode.None;
+        }
         SceneManager.LoadScene(sceneNumber);
+    }
+
+    public void ReloadLevel()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
     }
 }
